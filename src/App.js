1b6ac5ba006a605dacbd1de40db8bc2b9d22c620
Max <mjann@ucsc.edu>
2019-08-07 21:21:48 -0700

    jann tutoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import linked from './li.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header>
+      <h1>Jann Tutoring</h1>
+      <nav>
+        <a href="https://www.linkedin.com/in/maxjann/">
+        <img className="short" src={linked}></img>        
         </a>
+      </nav>
       </header>
+      <main>
+      <h2>Woodland Hills</h2>
+      <h3>Chess </h3>Beginner-1800
+      <h3>Math </h3>K-PreCalculus
+      <h4>$20/hr</h4>
+      <h2>805 940 5096</h2>
+      </main>
+      <footer> <a href="https://maxjann.com">Jann Software</a></footer>
     </div>
   );
 }
